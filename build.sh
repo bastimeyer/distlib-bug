b29c03768efa238dd8e78dd5f1d31bbb4cdeb746
bastimeyer <mail@bastimeyer.de>
2022-01-27 12:50:47 +0100

    manually run git describe --tags $hash

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 
 [[ -d distlib-git ]] || git clone https://github.com/pypa/distlib.git distlib-git
 
-while read -r hash describe; do
+while read -r hash; do
+  describe=$(git --git-dir ./distlib-git/.git describe --tags "${hash}")
   echo "Installing distlib ${describe} (${hash})"
   pip install "git+file://$(pwd)/distlib-git/.git@${hash}"
   echo "Building launchers with distlib ${describe} (${hash})"
   python ./build.py "${describe}"
   echo ""
-done < <(git --git-dir ./distlib-git/.git log "--pretty=format:%H %(describe:tags=true,abbrev=7)" 0.3.3..0.3.4)
+done < <(git --git-dir ./distlib-git/.git log "--pretty=format:%H" 0.3.3..0.3.4)
